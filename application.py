bb3a64a2f6a524655b2337574d210ca90c07ea9c
drmcarvalho <furiano.k@gmail.com>
2021-09-24 23:09:25 -0300

    rota inconsistencia semi pronta

--- application.py
+++ application.py
@@ -1,17 +1,29 @@
 from flask import Flask, jsonify, request
+from diffhunterfunctions import compare_database
 
 
+app = Flask(__name__)
+
+
+@app.route("/diffhunter/inconsistency")
 def inconsistency():
-    pass
+    body = request.get_json()
+    if not body:
+        return jsonify(error={"mensagem": "parametros invalidos"}), 400
+    result = compare_database(body["origin"]["uri"], body["target"]["uri"])
+    return jsonify(response={"diff": result.errors, "match": result.is_match})
 
 
+@app.route("/diffhunter/inconsistency_with_multiple_databases")
 def inconsistency_with_multiple_databases():
     pass
 
 
+@app.route("/diffhunter/diff_database")
 def diff_database():
     pass
 
 
+@app.route("/diffhunter/diff_with_multiple_databases")
 def diff_with_multiple_databases():
     pass
--- diffhunterfunctions.py
+++ diffhunterfunctions.py
@@ -1,11 +1,5 @@
 from sqlalchemydiff.comparer import compare
 
 
-def to_uri(user, password, database, host, port, client_name, driver_name=None):    
-    if driver_name:
-        return f'{client_name}+{driver_name}://{user}:{password}@{host}/{database}'
-    return f'{client_name}://{user}:{password}@{host}/{database}'
-
-
-def compare_database(uri_origin, url_target, ignores=None):
-    return compare(uri_origin, url_target, ignores=ignores)
+def compare_database(uri_origin, uri_target, ignores=None):
+    return compare(uri_origin, uri_target, ignores=ignores)
