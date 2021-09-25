c48c9fe20153e6c9ed1b66c0a1ef9f90eba02559
drmcarvalho <furiano.k@gmail.com>
2021-09-25 19:24:25 -0300

    ajustes

--- application.py
+++ application.py
@@ -20,20 +20,36 @@ def diff_database():
     target_uri = request.form.get("target_uri", None)
     origin_uri = request.form.get("origin_uri", None)
     if not target_uri:
-        return jsonify(error={"message": "Invalid URI"})
+        return jsonify(error={"message": "Invalid URI"}), 400
     if not origin_uri:
-        return jsonify(error={"message": "Invalid URI"})
+        return jsonify(error={"message": "Invalid URI"}), 400
     compare_result = compare_database(origin_uri, target_uri)
-    return jsonify(response={"diff": compare_result.errors,"match": compare_result.is_match})
+    return jsonify(result={"diff": compare_result.errors,"match": compare_result.is_match}), 200
 
 
 @app.route("/diffhunter/diff_with_multiple_databases")
 def diff_with_multiple_databases():
     origin_uri = request.form.get("origin_uri", None)
-    target_uri_list = request.form.getlist("target_uri_list")
+    target_uri_list = request.form.getlist("target_uri", None)
+    if not target_uri_list:
+        return jsonify(error={"message": "Invalid URI"}), 400
+    if not origin_uri:
+        return jsonify(error={"message": "Invalid list URI"}), 400
     result = {}
     result["diff_list"] = []
     for target_uri in target_uri_list:
         compare_result = compare_database(origin_uri, target_uri)
         result["diff_list"].append({"diff": compare_result.errors, "match": compare_result.is_match})
-    return jsonify(response=result)
+    return jsonify(result=result)
+
+
+with app.test_request_context():
+    l = request.form.getlist('lista', None)
+    print(l)
+
+
+@app.route('/test', methods=['post'])
+def test():
+    l = request.form.getlist('lista', None)
+    print(l)
+    return jsonify(test='test') 
