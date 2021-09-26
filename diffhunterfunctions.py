ea86ec2f1de5f50be16190c9574e42766cabd067
drmcarvalho <furiano.k@gmail.com>
2021-09-26 17:02:26 -0300

    calc

--- diffhunterfunctions.py
+++ diffhunterfunctions.py
@@ -1,5 +1,12 @@
 from sqlalchemydiff.comparer import compare
-
+from decimal import Decimal
+import math
 
 def compare_database(uri_origin, uri_target, ignores=None):
     return compare(uri_origin, uri_target, ignores=ignores)
+
+def calculate_value_inconsistency(total_diff=0):
+    return Decimal(total_diff / 100 / math.e)
+
+def determine_value_inconsistency(diff):
+    pass
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import diffhunterfunctions
 # def test_scheme_diff_database():
 #     v = validate_schema(body_scheme.scheme_diff_database, {"o": 55})
 #     print(v)
@@ -15,3 +16,65 @@
 # if errors:
 #     for error in errors:
 #         print(error)
+
+
+diff = {
+    "result": {
+        "diff": {
+            "enums": {},
+            "tables": {},
+            "tables_data": {
+                "pessoa": {
+                    "columns": {
+                        "diff": [
+                            {
+                                "key": "nome",
+                                "left": {
+                                    "comment": None,
+                                    "default": None,
+                                    "name": "nome",
+                                    "nullable": True,
+                                    "type": "VARCHAR(200)"
+                                },
+                                "right": {
+                                    "comment": None,
+                                    "default": None,
+                                    "name": "nome",
+                                    "nullable": True,
+                                    "type": "VARCHAR(100)"
+                                }
+                            }
+                        ],
+                        "left_only": [
+                            {
+                                "comment": None,
+                                "default": None,
+                                "name": "email",
+                                "nullable": True,
+                                "type": "VARCHAR(50)"
+                            }
+                        ]
+                    },
+                    "indexes": {
+                        "left_only": [
+                            {
+                                "column_names": [
+                                    "nome"
+                                ],
+                                "name": "nome",
+                                "unique": False
+                            }
+                        ]
+                    }
+                }
+            },
+            "uris": {
+                "left": "mysql+pymysql://root:@localhost/diffhuntermock2",
+                "right": "mysql+pymysql://root:@localhost/diffhuntermock1"
+            }
+        },
+        "match": False
+    }
+}
+
+print(diffhunterfunctions.calculate_value_inconsistency(690))
